37027a595e58a944dd5f55c59f9c991f77e276cd
baichenchen <baichenchen@iartden.com>
2017-05-24 19:12:00 +0800

    local commit

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -10,20 +10,49 @@ import Banner from './banner.jpg';
 import messages from './messages';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            isHoverdOn: false,
+            name:'woman',
+        };
+        this.handleHoverOn = this.handleHoverOn.bind(this);
+        this.handleHoverOut = this.handleHoverOut.bind(this);
+    }
+
+    handleHoverOn(event) {
+        this.setState(prevState => ({
+            isHoverdOn: true,
+            name: event,
+        }));
+    }
+
+    handleHoverOut(event) {
+        this.setState(prevState => ({
+            isHoverdOn: false,
+            name:'--',
+        }));
+    }
+
   render() {
     return (
       <div>
+          <div>
+              {this.state.name}
+          </div>
         <NavBar>
-            <HeaderLink to="/">
+            <HeaderLink onMouseOver={this.handleHoverOn.bind(this,'woman')} onMouseOut={this.handleHoverOut} to="/">
                 <FormattedMessage {...messages.woman} />
             </HeaderLink>
-            <HeaderLink to="/features">
+            <HeaderLink onMouseOver={this.handleHoverOn.bind(this,'man')} onMouseOut={this.handleHoverOut} to="/features">
                 <FormattedMessage {...messages.man} />
             </HeaderLink>
-            <HeaderLink to="/xiangfen">
+            <HeaderLink onMouseOver={this.handleHoverOn.bind(this,'xiangfen')} onMouseOut={this.handleHoverOut}  to="/xiangfen">
                 <FormattedMessage {...messages.xiangfen} />
             </HeaderLink>
-            <HeaderLink to="/caizhuang">
+            <HeaderLink onMouseOver={this.handleHoverOn.bind(this,'caizhuang')} onMouseOut={this.handleHoverOut}  to="/caizhuang">
                 <FormattedMessage {...messages.caizhuang} />
             </HeaderLink>
             <HeaderLink to="/hufu">
